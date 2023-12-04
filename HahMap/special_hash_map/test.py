0156224341578b909e68be3be4d35f28dcd8b708
env116 <107906220+ToshaTosha@users.noreply.github.com>
2023-12-05 01:20:05 +0800

    Update test.py

--- HahMap/special_hash_map/test.py
+++ HahMap/special_hash_map/test.py
@@ -42,11 +42,11 @@ class Test_SpecialHashMap:
         assert data_iloc.iloc[8] == 3
 
     def test_ploc(self, data_ploc):
-        assert data_ploc.ploc[">=1"] == "{1=10, 2=20, 3=30}"
-        assert data_ploc.ploc["<3"] == "{1=10, 2=20}"
-        assert data_ploc.ploc[">0, >0"] == "{(1, 5)=100, (5, 5)=200, (10, 5)=300}"
-        assert data_ploc.ploc[">=10, >0"] == "{(10, 5)=300}"
-        assert data_ploc.ploc["<5, >=5, >=3"] == "{(1, 5, 3)=400}"
+        assert data_ploc.ploc[">=1"] == "{1 = 10, 2 = 20, 3 = 30}"
+        assert data_ploc.ploc["<3"] == "{1 = 10, 2 = 20}"
+        assert data_ploc.ploc[">0, >0"] == "{(1, 5) = 100, (5, 5) = 200, (10, 5) = 300}"
+        assert data_ploc.ploc[">=10, >0"] == "{(10, 5) = 300}"
+        assert data_ploc.ploc["<5, >=5, >=3"] == "{(1, 5, 3) = 400}"
 
     def test_iloc_invalid_index(self, data_iloc):
         with pytest.raises(ValueError, match="Invalid index"):
@@ -55,3 +55,11 @@ class Test_SpecialHashMap:
     def test_ploc_invalid_condition(self, data_ploc):
         with pytest.raises(SyntaxError, match="invalid condition"):
             data_ploc.ploc[20]
+
+    def test_ploc_exception(self, data_ploc):
+        with pytest.raises(SyntaxError, match="Bad condition"):
+            data_ploc.ploc[">1, a"]
+
+    def test_ploc_bad_condition(self, data_ploc):
+        with pytest.raises(SyntaxError, match="Bad condition"):
+            data_ploc.ploc["<1, =>a, =1"]
